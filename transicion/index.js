dcab6f41ead3111ad91d9234895ba1d99a96756a
huntergoc <joyadelsurspa@gmail.com>
2021-07-03 00:00:55 -0400

    sitio web

---
+++ transicion/index.js
@@ -0,0 +1,55 @@
+window.sr = ScrollReveal();
+  sr.reveal('.hero__texts', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.hero__img', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.instructor__container', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-400px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.about__course', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-500px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.about__img', {
+    duration: 3000,
+    rotate: {
+        x: 1,
+        y: 180,
+    }   
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.logo', {
+    duration: 5000,
+    rotate: {
+        x: 1,
+        y: 180,
+    }   
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-500px'
+  });
+
+  
+
